abec1d8f02d45ca657c603663b1046edb64a1dc0
Ibrahim Rouis <ibrahimrouisdev@proton.me>
2025-07-14 14:51:32 +0100

    Switch to demo-project for emulators only

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,10 @@
 // Web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDaO8RDEMjQqxOkMUUsmKhesFZfFohQ7bc',
-  authDomain: 'places-app-9d9b0.firebaseapp.com',
-  projectId: 'places-app-9d9b0',
-  storageBucket: 'places-app-9d9b0.firebasestorage.app',
-  messagingSenderId: '622401671508',
-  appId: '1:622401671508:web:d9bf0c3ba5c035403438c1',
+  apiKey: 'AIzaSyD3m0-DEMO_KEY_1234567890',
+  authDomain: 'demo-project.firebaseapp.com',
+  projectId: 'demo-project',
+  storageBucket: 'demo-project.appspot.com',
+  messagingSenderId: '1234567890',
+  appId: '1:1234567890:web:abcdef123456',
+  measurementId: 'G-ABCDEFG123',
 };
