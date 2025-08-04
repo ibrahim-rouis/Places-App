c0b02c14ed234d929133e3f9ed20f37e84f46bbe
Ibrahim Rouis <ibrahimrouisdev@proton.me>
2025-08-04 16:55:17 +0100

    Implement search functionality in MySearchBar component

--- src/components/molecules/SearchBar.tsx
+++ src/components/molecules/SearchBar.tsx
@@ -1,16 +1,36 @@
 import { Search } from 'lucide-react';
 import { Input } from '../ui/input';
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 function MySearchBar() {
+  const [searchText, setSearchText] = useState('');
+  const navigate = useNavigate();
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      const params = new URLSearchParams();
+      params.append('q', searchText.trim());
+      navigate(`/explore?${params}`);
+    },
+    [searchText, navigate],
+  );
+
   return (
-    <div className="relative mt-2 flex items-center hover:text-sky-500 md:mx-1 md:w-45 md:hover:text-inherit">
+    <form
+      onSubmit={onSubmit}
+      className="relative mt-2 flex items-center hover:text-sky-500 md:mx-1 md:w-45 md:hover:text-inherit"
+    >
       <Search className="size-7 md:absolute md:left-2 md:size-5" />
       <Input
         type="search"
         placeholder="Search"
         className="hidden md:block md:w-full md:p-2 md:pl-9"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
       />
-    </div>
+      <input type="submit" hidden disabled={!searchText.trim()} />
+    </form>
   );
 }
 export default MySearchBar;
